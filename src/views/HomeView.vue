7878974bf940807f36fd4809a63fa8a3b80f2bb9
HJY <jw.hwang@twolinecode.com>
2023-05-08 17:26:43 +0900

    여러링크 예외처리

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -76,7 +76,7 @@ export default {
     playListId () {
       const idList = [];
       Object.keys(this.playList).forEach(key => {
-        idList.push(this.playList[key].link.replace('https://www.youtube.com/watch?v=', ''));
+        idList.push(this.getCode(this.playList[key].link));
       });
       return idList;
     },
@@ -146,10 +146,24 @@ export default {
       });
     },
 
-    getCode (item) { //전체 링크에서 영상 코드만 추출
-      return item.replace('https://www.youtube.com/watch?v=', '');
+    getCode (url) { //전체 링크에서 영상 코드만 추출
+      let videoID = '';
+      if (url.includes('youtu.be/')) {
+        videoID = url.split('youtu.be/')[1];
+      } else if (url.includes('youtube.com/watch?v=')) {
+        videoID = url.split('youtube.com/watch?v=')[1];
+      } else if (url.includes('youtube.com/embed/')) {
+        videoID = url.split('youtube.com/embed/')[1];
+      } else if (url.includes('youtube.com/v/')) {
+        videoID = url.split('youtube.com/v/')[1];
+      } 
+      const ampersandPosition = videoID.indexOf('&');
+      if (ampersandPosition !== -1) {
+        videoID = videoID.substring(0, ampersandPosition);
+      }
+      return videoID;
     },
-
+    
     getTitle (key) {
       const split = key.split('-');
       return split[1];

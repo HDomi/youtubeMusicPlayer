fd90131080f8e73c73f897c36e2d550cd312067a
HDomi <84692508+HDomi@users.noreply.github.com>
2023-05-08 10:40:09 +0900

    test

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<style>
+body {
+  overflow-x: hidden;
+  background: rgb(32, 32, 36);
+  color: #fff;
+  margin: 0 auto;
+  padding: 0;
+  font-family: 'Pretendard', -apple-system, BlinkMacSystemFont, system-ui, Roboto, 'Helvetica Neue', 'Segoe UI', 'Apple SD Gothic Neo', 'Noto Sans KR', 'Malgun Gothic', sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  width: 100%;
+  height: 100%;
+}
+div {
+  box-sizing: border-box;
+}
+body::-webkit-scrollbar {
+  width: 8px;  /* 스크롤바의 너비 */
+}
+body::-webkit-scrollbar-thumb {
+  height: 30%; /* 스크롤바의 길이 */
+  background: #5d5d5d; /* 스크롤바의 색상 */
+  border-radius: 10px;
+}
+body::-webkit-scrollbar-track {
+  background: rgba(91, 91, 91, 0.1);  /*스크롤바 뒷 배경 색상*/
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,330 @@
+<template>
+  <div class="wrap stars">
+    <div class="small"></div>
+    <div class="medium"></div>
+    <div class="big"></div>
+    <div class="controlBar-wrap">
+      <div class="flux">Music Player</div>
+      <div class="description">바로 밑에 있는 목록이 재생되고 재생이 완료된 노래는 삭제됩니다.</div>
+      <youtube-media :video-id="videoId" @ready="ready" @playing="playing" @ended="change" :player-vars="{autoplay: 1}" style="position: absolute; left: -9999px; top: -9999px; width: 0; height: 0"></youtube-media>
+      <div class="playing-info-wrap">
+        <div v-if="nowSingTitle.length" class="play-title">{{ nowSingTitle }}</div>
+        <div v-else class="play-title">재생을 눌러주세요.</div>
+      </div>
+      <div class="btn-wrap">
+        <img v-if="isPlay" @click="pause" src="../assets/images/pause-white.png"/>
+        <img v-else @click="play" src="../assets/images/play-white.png"/>
+        <img @click="change" src="../assets/images/skip-white.png"/>
+      </div>
+    </div>
+    <div class="add-sing-wrap">
+      <span v-if="!isAddSing" @click="isAddSing = true">노래 추가하기</span>
+      <div v-if="isAddSing" class="add-sing-info">
+        <div class="set-info">
+          <input v-model="addSingTitle" placeholder="제목" type="text"/>
+          <input v-model="addSingLink" placeholder="링크" type="text" style="margin-top: 7px;"/>
+        </div>
+        <div class="add-btn" @click="addSing">+</div>
+      </div>
+    </div>
+    <div class="playList-wrap">
+      <div v-if="!playList.length" class="not-set-sing">추가된 노래가 없습니다!</div>
+      <div class="playList-item" :class="{nowPlaying: getNowPlay(item.link)}" v-for="(item, index) in playList" :key="`play-${index}`">
+        <div class="info-wrap" @click="onClickChange(item.link)">
+          <div class="name">{{ item.label }}</div>
+          <div class="link">{{ item.link }}</div>
+        </div>
+        <div class="control-wrap">
+          <div v-if="getNowPlay(item.link) && isPlay" @click="pause" class="btn">
+            <img src="../assets/images/pause.svg"/>
+          </div>
+          <div v-else class="btn" @click="deleteItem(item.link)">
+            <div class="delete">-</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+// import { getTimeFromURL } from 'vue-youtube-embed' 
+export default {
+  name: "HomeView",
+  components: {
+  },
+  data() {
+    return {
+      isPlay: false,
+      isAddSing: false,
+      playList: [
+        {label: '몇번째', link: 'https://www.youtube.com/watch?v=Yagz8h0jppA'},
+        {label: '능소화', link: 'https://www.youtube.com/watch?v=hbeSbiaHKbw'},
+        {label: '발밤발밤', link: 'https://www.youtube.com/watch?v=I9v35zgOkZU'},
+        {label: '그러나', link: 'https://www.youtube.com/watch?v=gZLoabyxzes'},
+        {label: 'Wash Away', link: 'https://www.youtube.com/watch?v=OTRGehVo_JM'},
+        {label: 'Fan', link: 'https://www.youtube.com/watch?v=eINjPkO7w4M'},
+        {label: 'Officially Missing You', link: 'https://www.youtube.com/watch?v=lrSQPqD69AI'},
+        {label: '박하사탕', link: 'https://www.youtube.com/watch?v=-85-sjG_BiQ'},
+      ],
+      videoId: '',
+      nowSingTitle: '',
+      nowSingTime: '',
+
+      addSingTitle: '',
+      addSingLink: '',
+    };
+  },
+  computed: {
+    playListId () {
+      const idList = this.playList.map(l => l.link.replace('https://www.youtube.com/watch?v=', ''));
+      return idList;
+    },
+  },
+  // watch: {
+  //   playList () {
+  //     this.playListId = this.playList.map(l => l.link.replace('https://www.youtube.com/watch?v=', ''));
+  //   }
+  // },
+  methods: {
+    ready (event) { //노래 준비됨
+      this.isPlay = false;
+      this.player = event.target;
+    },
+    playing (event) { //노래 시작됨
+      this.isPlay = true;
+      const playingData = this.player.getVideoData();
+      this.nowSingTitle = playingData.title;
+      console.log('@@@@@@@@@@@@',event);
+      console.log('노래가 재생됩니다.\n', this.nowSingTitle);
+    },
+    change () { //노래바꾸기
+      this.isPlay = true;
+      const nowIndex = this.playListId.indexOf(this.videoId, 0);
+      const nextNum = this.playListId.length <= nowIndex + 1 ? 0 : nowIndex + 1;
+      this.videoId = this.playListId[nextNum];
+      this.playList.splice(nowIndex, 1);
+    },
+    play () { //재생
+      this.isPlay = true;
+      this.player.playVideo();
+    },
+    pause () { //일시정지
+      this.isPlay = false;
+      this.player.pauseVideo();
+    },
+    getCode (item) { //전체 링크에서 영상 코드만 추출
+      return item.replace('https://www.youtube.com/watch?v=', '');
+    },
+    getNowPlay (item) { //지금 플레이중인지, true/false
+      const id = this.getCode(item);
+      return id === this.videoId;
+    },
+    onClickChange (item) { //아이템을 눌러 노래 바꾸기
+      this.videoId = this.getCode(item);
+    },
+    addSing() { //리스트에 아이템 추가
+      this.playList.push({label: this.addSingTitle, link: this.addSingLink});
+      this.addSingTitle = this.addSingLink = '';
+      this.isAddSing = false;
+    },
+    deleteItem (item) { //리스트에서 아이템 삭제
+      const nowIndex = this.playList.findIndex(l => l.link === item);
+      this.playList.splice(nowIndex, 1);
+    }
+  },
+  mounted() {
+    this.videoId = this.playListId[0]; //제일 첫번째 목록에 있는 노래 자동재생
+    this.isPlay = true;
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+@import url(../assets/styles/stars.css);
+  @font-face {
+    font-family: neon;
+    src: url(../assets/fonts/neon.ttf);
+  }
+  .wrap{
+    width: 100%;
+    height: 100%;
+    position: relative;
+  }
+  .controlBar-wrap{
+    width: 100%;
+    height: 150px;
+    box-shadow: 0 14px 28px rgba(255,255,255,0.01), 0 10px 10px rgba(255,255,255,0.02);
+    background: rgb(32, 32, 36);
+    align-items: center;
+    justify-content: center;
+    display: flex;
+    flex-direction: column;
+    position: relative;
+    .playing-info-wrap{
+      .play-title{
+        font-size: 23px;
+        font-weight: bold;
+      }
+    }
+    .description{
+      font-size: 13px;
+    }
+    .flux {
+      position: absolute;
+      top: 10px;
+      left: 15px;
+      font-family: neon;
+      color: rgb(107, 176, 255);
+      font-size: 21px;
+      text-shadow: 0 0 1px rgb(60, 150, 253);
+    }
+    .flux {
+        animation: flux 3s linear infinite;
+        -moz-animation: flux 3s linear infinite;
+        -webkit-animation: flux 3s linear infinite;
+        -o-animation: flux 3s linear infinite;
+    }
+    @keyframes flux {
+        0%,
+        100% {
+          text-shadow: 0 0 1vw #1041FF, 0 0 3vw #1041FF, 0 0 10vw #1041FF, 0 0 10vw #1041FF, 0 0 .4vw #8BFDFE, .5vw .5vw .1vw #147280;
+          color: #28D7FE;
+        }
+        50% {
+          text-shadow: 0 0 .5vw #082180, 0 0 1.5vw #082180, 0 0 5vw #082180, 0 0 5vw #082180, 0 0 .2vw #082180, .5vw .5vw .1vw #0A3940;
+          color: #146C80;
+        }
+    }
+    .btn-wrap{
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+      justify-content: space-between;
+      width: 70px;
+      height: 40px;
+      margin-top: 20px;
+      img{
+        width: 20px;
+        height: auto;
+        cursor: pointer;
+      }
+    }
+  }
+  .add-sing-wrap{
+    width: 100%;
+    min-height: 50px;
+    margin: 20px 0;
+    padding: 13px 30px;
+    background: rgba(187, 187, 187, 0.13);
+    border: 1px solid #fff;
+    span{
+      cursor: pointer;
+      font-size: 17px;
+      font-weight: bold;
+    }
+    .add-sing-info{
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+      justify-content: space-between;
+      .set-info{
+        display: flex;
+        flex-direction: column;
+        width: calc(100% - 96px);
+        input{
+          width: 100%;
+          background: rgba(248, 248, 248, 0.682);
+          &::placeholder{
+            color: #000;
+          }
+          &:focus{
+            outline: none !important;
+            border-color: rgb(107, 176, 255);
+            box-shadow: 0 0 10px rgb(60, 150, 253);
+          }
+        }
+      }
+      .add-btn{
+        color: #000;
+        cursor: pointer;
+        background: #fff;
+        text-align: center;
+        border-radius: 10px;
+        width: 40px;
+        height: 40px;
+        color: #000;
+        line-height: 30px;
+        font-weight: bold;
+        font-size: 40px;
+      }
+    }
+  }
+  .playList-wrap{
+    width: 100%;
+    height: 100%;
+    padding: 0 30px;
+    .nowPlaying{
+      background: rgba(80, 162, 255, 0.53)!important;
+    }
+    .playList-item{
+      border: 1px solid #fff;
+      cursor: pointer;
+      background: rgba(234, 234, 234, 0.053);
+      margin-top: 20px;
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+      justify-content: space-between;
+      height: 100px;
+      padding: 0 30px;
+      border-radius: 15px;
+      .info-wrap{
+        text-align: left;
+        .name{
+          font-size: 22px;
+          font-weight: bold;
+        }
+        .link{
+        }
+      }
+      .control-wrap{
+        .btn{
+          cursor: pointer;
+          background: #fff;
+          text-align: center;
+          border-radius: 10px;
+          width: 40px;
+          height: 40px;
+          img{
+            width: 25px;
+            height: 25px;
+            margin-top: 7px;
+            cursor: pointer;
+          }
+          .delete{         
+            color: #000;
+            line-height: 30px;
+            font-weight: bold;
+            font-size: 40px;
+            
+          }
+        }
+      }
+    }
+  }
+  @media (max-width: 1070px) {
+    .controlBar-wrap{
+      height: 175px;
+      .flux{
+        top: 0px;
+        left: 0px;
+        position: relative;
+        margin-bottom: 20px;
+      }
+      .btn-wrap{
+        margin-top: 0px;
+      }
+    }
+  }
+</style>
